7d6e118449e79d3cf56c4d1efcacdb011315feed
YefersonMauricioDiazChogo21 <yefersondiaz2001@gmail.com>
2024-07-31 13:55:19 -0500

    docs :sparkles: documentacion

--- ddl.sql
+++ ddl.sql
@@ -2,14 +2,22 @@ DROP DATABASE IF EXISTS myshop;
 CREATE DATABASE myshop;
 USE myshop;
 
+CREATE TABLE ciudades(
+    id_ciudad INT,
+    nombre VARCHAR(20),
+    CONSTRAINT pk_cliente_id PRIMARY KEY (id_ciudad)
+);
+
 CREATE TABLE clientes(
     id VARCHAR(20),
     nombre VARCHAR(14),
     apellidos VARCHAR(100),
     celular DECIMAL(10,0),
     direccion VARCHAR(80),
+    id_ciudad INT,
     correo_electonico VARCHAR(70),
-    CONSTRAINT pk_cliente_id PRIMARY KEY (id)
+    CONSTRAINT pk_cliente_id PRIMARY KEY (id),
+    CONSTRAINT fk_id_ciudad FOREIGN KEY (id_ciudad) REFERENCES ciudades(id_ciudad)
 );
 
 CREATE TABLE categorias(
@@ -19,22 +27,24 @@ CREATE TABLE categorias(
     CONSTRAINT pk_categoria_id PRIMARY KEY (id_categoria)
 );
 
+CREATE TABLE proveedores(
+    id_proveedor INT,
+    nombre VARCHAR(20),
+    CONSTRAINT pk_proveedor_id PRIMARY KEY (id_proveedor)
+);
+
 CREATE TABLE productos(
     id_producto INT,
     nombre VARCHAR(45),
     id_categoria INT,
+    id_proveedor INT,
     codigo_barras VARCHAR(150),
     precio_venta DECIMAL(16,2),
     cantidad_stock INT,
     estado TINYINT,
     CONSTRAINT pk_producto_id PRIMARY KEY (id_producto),
-    CONSTRAINT fk_id_categoria FOREIGN KEY (id_categoria) REFERENCES categorias(id_categoria)
-);
-
-CREATE TABLE medio_pago(
-    id_medio_pago INT,
-    descripcion VARCHAR(20),
-    CONSTRAINT pk_medio_pago_id PRIMARY KEY (id_medio_pago)
+    CONSTRAINT fk_id_categoria FOREIGN KEY (id_categoria) REFERENCES categorias(id_categoria),
+    CONSTRAINT fk_id_proveedor FOREIGN KEY (id_proveedor) REFERENCES proveedores(id_proveedor)
 );
 
 CREATE TABLE compras(
@@ -45,8 +55,7 @@ CREATE TABLE compras(
     comentario VARCHAR(300),
     estado CHAR(1),
     CONSTRAINT pk_compra_id PRIMARY KEY (id_compra),
-    CONSTRAINT fk_id_cliente FOREIGN KEY (id) REFERENCES clientes(id),
-    CONSTRAINT fk_medio_pago FOREIGN KEY (medio_pago) REFERENCES medio_pago(id_medio_pago)
+    CONSTRAINT fk_id_cliente FOREIGN KEY (id_cliente) REFERENCES clientes(id)
 );
 
 CREATE TABLE compras_productos(
--- ddm.sql
+++ ddm.sql
@@ -1,8 +1,12 @@
-INSERT INTO clientes ( id ,nombre ,apellidos ,celular ,direccion, correo_electonico) VALUES
-    (1005539417,"Yeferson", "Diaz Chogo",3143807720,"Barrio Las Granjas","Yefer@gmail.com"),
-    (1005539418,"Laura", "Torres",3143807721,"Palomitas","laura@gmail.com"),
-    (1005539419,"Valentina", "Herrera",3143807722,"Florida","valentinaH@gmail.com"),
-    (1005539420,"Karol", "Ramirez",3143807723,"Mutis","KarolR@gmail.com");
+INSERT INTO ciudades (id_ciudad, nombre) VALUES
+    (1, "Bucaramanga"),
+    (2, "Bogota");
+
+INSERT INTO clientes ( id ,nombre ,apellidos ,celular ,direccion,id_ciudad, correo_electonico) VALUES
+    (1005539417,"Yeferson", "Diaz Chogo",3143807720,"Barrio Las Granjas",1,"Yefer@gmail.com"),
+    (1005539418,"Laura", "Torres",3143807721,"Palomitas",1,"laura@gmail.com"),
+    (1005539419,"Valentina", "Herrera",3143807722,"Florida",2,"valentinaH@gmail.com"),
+    (1005539420,"Karol", "Ramirez",3143807723,"Mutis",1,"KarolR@gmail.com");
 
 INSERT INTO categorias( id_categoria ,descripcion ,estado) VALUES
     (1,"Aseo personal",1),
@@ -12,19 +16,34 @@ INSERT INTO categorias( id_categoria ,descripcion ,estado) VALUES
     (5,"Lacteos",2),
     (6,"Congelados",1);
 
-INSERT INTO productos(id_producto, nombre, id_categoria, codigo_barras, precio_venta, cantidad_stock, estado) VALUES
-    (1,"Jabon Protex",1,"",2000,20,1),
-    (2,"Fabuloso",2,"",3000,15,1),
-    (3,"Arroz Diana",3,"",3800,22,1),
-    (4,"Coca-cola personal",4,"",3000,20,1),
-    (5,"Alqueria litro",5,"",4500,25,1),
-    (6,"Perniles de pollo",6,"",7800,10,1),
-    (7,"Saviloe",4,"",3500,20,1),
-    (8,"Panela",3,"",2500,20,1);
+INSERT INTO proveedores(id_proveedor,nombre) VALUES 
+    (1,"Protex"),
+    (2,"Panorama"),
+    (3,"Mi graja");
+
+INSERT INTO productos(id_producto, nombre, id_categoria,id_proveedor, codigo_barras, precio_venta, cantidad_stock, estado) VALUES
+    (1,"Jabon Protex",1,1,"",2000,20,1),
+    (2,"Fabuloso",2,2,"",3000,15,1),
+    (3,"Arroz Diana",3,2,"",3800,22,1),
+    (4,"Coca-cola personal",4,2,"",3000,20,1),
+    (5,"Alqueria litro",5,2,"",4500,25,1),
+    (6,"Perniles de pollo",6,3,"",7800,10,1),
+    (7,"Saviloe",4,2,"",3500,20,1),
+    (8,"Panela",3,3,"",2500,20,1);
 
 INSERT INTO compras(id_compra, id_cliente, fecha, medio_pago, comentario, estado) VALUES
-    (1,1005539417,"2024-07-02","E","",1);
+    (1,1005539417,"2024-07-02","E","",1),
+    (2,1005539418,"2024-07-10","T","",1),
+    (3,1005539419,"2024-03-05","E","",1),
+    (4,1005539420,"2024-04-10","E","",1),
+    (5,1005539417,"2024-07-12","T","",1);
 
 INSERT INTO compras_productos(id_compra, id_producto, cantidad, total, estado) VALUES
     (1,1,2,4000,1),
-    (1,2,3,9000,1);
+    (1,2,3,9000,1),
+    (2,4,1,3000,1),
+    (2,6,1,7800,1),
+    (3,3,5,19500,1),
+    (4,5,2,9000,1),
+    (4,8,1,2500,1),
+    (5,1,3,6000,1);
